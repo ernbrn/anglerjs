8e5a8ab6507ff209e90be7dcbd293bb7679ea605
Erin Brown <erinbrown@ijoinsolutions.com>
2015-09-03 20:22:29 -0400

    ANGLERFISH

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -21,8 +21,43 @@ angular.module('myApp.view1', ['ngRoute'])
 		}
   };
 })
-
+.directive('addFish', function() {
+  return {
+		scope: {
+			fishy: '='
+		},
+		restrict: 'A',
+		template: '<img class="freakyFish">',
+		link: function(scope, element, attrs){
+			console.log(element);
+			var myImgTag = element.find('img');
+			myImgTag.attr('src', scope.fishy);
+		}
+  };
+})
 .controller('View1Ctrl', ['$scope', function($scope) {
+	$scope.showMe = false;
+	$scope.trout = "bluefish";
 
+	$scope.myFish = [
+		'crappy',
+		'catfish',
+		'minnow'
+	];
 
+	$scope.imageFish = 'http://i.imgur.com/6kGBW01.jpg';
+
+	$scope.addAFish = function () {
+		$scope.myFish.push($scope.moreFish);
+		$scope.showFreakyFish($scope.moreFish);
+	};
+
+	$scope.showFreakyFish = function (fish) {
+		if (fish.toLowerCase() === 'anglerfish') {
+			$scope.showMe = true;
+		}
+		else {
+			$scope.showMe = false;
+		}
+	};
 }]);
